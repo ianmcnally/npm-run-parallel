4f554f29bf99bc61a31509d910408b5287259012
Ian McNally <imcnally@stridenyc.com>
2016-02-16 17:50:20 -0500

    print error on fail

--- run.js
+++ run.js
@@ -3,13 +3,16 @@ import childProcess from 'child_process'
 
 const exec = promisify(childProcess.exec)
 
-const runTask = (task) => exec('npm run ' + task).then(() => console.log('ran: ' + task))
+const runTask = (task) => (
+  exec('npm run ' + task)
+    .then(() => console.log('ran: ' + task))
+)
 
 const runTasks = (...tasks) => {
   console.log('tasks: ', tasks)
   Promise.all(tasks.map(runTask))
     .then(() => console.log('done'))
-    .catch(() => console.log('error'))
+    .catch(error => console.error('error: ', error))
 }
 
 runTasks('test:one', 'test:two')
