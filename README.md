28f755addbbbfc9b699ee0c31833097324637074
Ian McNally <imcnally@stridenyc.com>
2016-02-16 17:37:50 -0500

    add working demo

---
+++ README.md
@@ -0,0 +1,1 @@
+Run npm tasks in parallel and exit when they are all done.
---
+++ run.js
@@ -0,0 +1,16 @@
+import { Promise, promisify } from 'bluebird'
+import childProcess from 'child_process'
+
+const exec = promisify(childProcess.exec)
+
+const runTask = (task) => exec('npm run ' + task).then(() => console.log('ran: ' + task))
+
+const runTasks = (...tasks) => {
+  console.log('tasks: ', tasks)
+  Promise.all(tasks.map(runTask))
+    .then(() => console.log('done'))
+    .catch(() => console.log('error'))
+}
+
+runTasks('test:one', 'test:two')
+
