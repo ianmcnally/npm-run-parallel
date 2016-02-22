2ea79c0dab10e9aebcd42521ca2d3605d2acd3bc
Ian McNally <imcnally@stridenyc.com>
2016-02-22 11:50:05 -0500

    cleanup skipped tests

--- test/cli_test.js
+++ test/cli_test.js
@@ -1,26 +1,6 @@
-import { shell } from 'execa'
-import { expect } from 'chai'
-import pkg from '../package.json'
-
 xdescribe('lib/cli', () => {
-  const taskThatIsLonger = '__test_fixture_longer__'
-  const taskThatIsShorter = '__test_fixture_shorter__'
-  let output
-
-  before(() => {
-    output = shell(`node ${pkg.bin} ${taskThatIsLonger} ${taskThatIsShorter}`)
-
-    return output
-  })
 
   it('runs a task', () => {
-    return output.then(({ stdout }) => {
-      expect(stdout).to.contain([
-        `ran: ${taskThatIsShorter}`,
-        `ran: ${taskThatIsLonger}`,
-        'done'
-      ].join('\n'))
-    })
   })
 
 })
--- test/run_test.js
+++ test/run_test.js
@@ -1,7 +1,6 @@
 import { runTasks } from '../lib/run'
-import execa from 'execa'
-import { expect } from 'chai'
-import { stub } from 'sinon'
+// import { expect } from 'chai'
+// import { stub } from 'sinon'
 
 xdescribe('lib/run', () => {
 
@@ -12,19 +11,10 @@ xdescribe('lib/run', () => {
     ]
 
     before(() => {
-      stub(execa, 'shell').returns(Promise.resolve())
-
       return runTasks(tasks)
     })
 
-    after(() => {
-      execa.shell.restore()
-    })
-
     it('executes an `npm run` for each task', () => {
-      tasks.forEach(task => {
-        expect(execa.shell).to.have.been.calledWith(`npm run ${task}`)
-      })
     })
 
   })
