c36551881979108806fd15e81ad94abf7d436a7e
Ian McNally <imcnally@stridenyc.com>
2016-02-23 18:40:06 -0500

    test the cli

--- test/cli_test.js
+++ test/cli_test.js
@@ -1,6 +1,25 @@
-xdescribe('lib/cli', () => {
+import execa from 'execa'
+import pkg from '../package.json'
+import { expect } from 'chai'
 
-  it('runs a task', () => {
+describe('lib/cli', () => {
+  const tasks = [
+    '__test_fixture__',
+    '__test_fixture_2__'
+  ]
+  let stdout
+
+  before(() => {
+    return execa('node', [pkg.bin, ...tasks])
+      .then(output => {
+        stdout = output.stdout
+      })
+  })
+
+  it('launches the task runner', () => {
+    tasks.forEach(task => {
+      expect(stdout).to.contain(`${pkg.name}@${pkg.version} ${task}`)
+    })
   })
 
 })
