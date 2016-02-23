575ef6d4109138e59fdaa8a3952dc02378b02646
Ian McNally <imcnally@stridenyc.com>
2016-02-23 18:25:37 -0500

    add test for `runTasks`. closes #4.

--- test/run_test.js
+++ test/run_test.js
@@ -1,20 +1,30 @@
 import { runTasks } from '../lib/run'
-// import { expect } from 'chai'
-// import { stub } from 'sinon'
+import childProcess from 'child_process'
+import { expect } from 'chai'
+import { spy } from 'sinon'
 
-xdescribe('lib/run', () => {
+describe('lib/run', () => {
 
   describe('runTasks', () => {
     const tasks = [
-      'this:thing',
-      'that:thing'
+      '__test_fixture__',
+      '__test_fixture_2__'
     ]
 
     before(() => {
-      return runTasks(tasks)
+      spy(childProcess, 'exec')
+
+      runTasks(tasks)
+    })
+
+    after(() => {
+      childProcess.exec.restore()
     })
 
     it('executes an `npm run` for each task', () => {
+      tasks.forEach(task => {
+        expect(childProcess.exec).to.have.been.calledWith(`npm run ${task}`)
+      })
     })
 
   })
